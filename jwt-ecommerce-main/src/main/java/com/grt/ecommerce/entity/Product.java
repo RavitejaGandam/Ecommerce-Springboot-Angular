8380f3f5bccdb80f0e44a35ec29d59a980c3f7f0
Raviteja Gandam <gandamraviteja@gmail.com>
2024-05-10 18:15:37 +0530

    Entity and Repo

---
+++ jwt-ecommerce-main/src/main/java/com/grt/ecommerce/entity/Product.java
@@ -0,0 +1,57 @@
+package com.grt.ecommerce.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer productId;
+    private String productName;
+    private String productDescription;
+    private Double productDiscountedPrice;
+    private Double getProductActualPrice;
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public Double getProductDiscountedPrice() {
+        return productDiscountedPrice;
+    }
+
+    public void setProductDiscountedPrice(Double productDiscountedPrice) {
+        this.productDiscountedPrice = productDiscountedPrice;
+    }
+
+    public Double getGetProductActualPrice() {
+        return getProductActualPrice;
+    }
+
+    public void setGetProductActualPrice(Double getProductActualPrice) {
+        this.getProductActualPrice = getProductActualPrice;
+    }
+}
